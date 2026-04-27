0690a35cd7192e0ec8fc2d03ba2cc4451d93860a
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-27 16:50:33 -0500

    chore(structure): add register client use case placeholder

---
+++ src/software_fj_reservation_system/application/registrar_cliente.py
@@ -0,0 +1,9 @@
+"""
+Caso de uso: registrar cliente.
+
+Este módulo coordinará el registro de clientes en el sistema.
+Validará los datos recibidos, creará el cliente y lo guardará
+en el repositorio correspondiente.
+
+La implementación completa se realizará en la fase de desarrollo.
+"""
