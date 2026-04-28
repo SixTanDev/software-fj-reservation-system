701ac19111a76e6059e3c447178364ecf66e74e1
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:16:29 -0500

    chore: rename registrar_cliente to register_client

---
+++ src/software_fj_reservation_system/application/register_client.py
@@ -0,0 +1,7 @@
+"""
+Use case: Registering a client.
+
+This module will coordinate client registration in the system. It will validate the received data, create the client, and save it in the corresponding repository.
+
+Full implementation will be carried out during the development phase.
+"""
