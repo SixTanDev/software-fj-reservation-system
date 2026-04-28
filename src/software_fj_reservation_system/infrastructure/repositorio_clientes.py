4f13ff37c9c3fca1c9d8aafa4d8c796b5bca7fc2
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:17:47 -0500

    chore: rename repositorio_clientes to client_repository

--- src/software_fj_reservation_system/infrastructure/repositorio_clientes.py
+++
@@ -1,8 +0,0 @@
-"""
-Repositorio de clientes en memoria.
-
-Este módulo almacenará clientes usando listas internas,
-sin utilizar base de datos.
-
-La implementación completa se realizará en la fase de desarrollo.
-"""
