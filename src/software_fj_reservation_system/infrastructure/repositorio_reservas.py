188b6ec27f22bba297e38156011f9974a0eaecd2
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:18:33 -0500

    chore: rename repositorio_reservas to reservation_repository

--- src/software_fj_reservation_system/infrastructure/repositorio_reservas.py
+++
@@ -1,8 +0,0 @@
-"""
-Repositorio de reservas en memoria.
-
-Este módulo almacenará las reservas creadas usando listas internas,
-sin utilizar base de datos.
-
-La implementación completa se realizará en la fase de desarrollo.
-"""
