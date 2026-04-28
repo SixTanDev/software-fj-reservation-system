c6048632ed6166f874c2db6597adf27f20bf99c7
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:17:47 -0500

    chore: rename repositorio_clientes to client_repository

---
+++ src/software_fj_reservation_system/infrastructure/client_repository.py
@@ -0,0 +1,8 @@
+"""
+In-memory customer repository.
+
+This module will store customers using internal lists,
+without using a database.
+
+Full implementation will be carried out during the development phase.
+"""
