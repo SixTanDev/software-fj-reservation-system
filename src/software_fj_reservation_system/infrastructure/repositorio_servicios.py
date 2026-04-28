d2ffef8820eab9194e24d490b9d76841529cf5f4
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:19:35 -0500

    chore: rename repositorio_servicios to service_repository

--- src/software_fj_reservation_system/infrastructure/repositorio_servicios.py
+++
@@ -1,8 +0,0 @@
-"""
-Repositorio de servicios en memoria.
-
-Este módulo almacenará los servicios disponibles usando listas internas,
-sin utilizar base de datos.
-
-La implementación completa se realizará en la fase de desarrollo.
-"""
