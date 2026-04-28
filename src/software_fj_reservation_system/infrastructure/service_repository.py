6dd0a6fef1711f2db6860a0febc71b4d1cc2feb7
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:19:35 -0500

    chore: rename repositorio_servicios to service_repository

---
+++ src/software_fj_reservation_system/infrastructure/service_repository.py
@@ -0,0 +1,8 @@
+"""
+In-memory service repository.
+
+This module will store available services using internal lists,
+without using a database.
+
+Full implementation will be carried out during the development phase.
+"""
