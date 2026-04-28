c69fa907bf2636c5b82b1bb0a4e52e60f4ccd1bc
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:12:25 -0500

    chore: rename crear_servicio to create_service

---
+++ src/software_fj_reservation_system/application/create_service.py
@@ -0,0 +1,7 @@
+"""
+Use case: Create a service.
+
+This module will coordinate the creation of room services, equipment rentals, and specialized consulting.
+
+Full implementation will take place during the development phase.
+"""
