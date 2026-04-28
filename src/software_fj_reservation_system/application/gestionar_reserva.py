563535b238340e3bea9931de4dbf065e99b3e3da
ALEJOGT23 <jagonzaleztap@unadvirtual.edu.co>
2026-04-28 15:15:36 -0500

    chore: rename gestionar_reserva to manage_reservation

--- src/software_fj_reservation_system/application/gestionar_reserva.py
+++
@@ -1,8 +0,0 @@
-"""
-Caso de uso: gestionar reserva.
-
-Este módulo coordinará operaciones sobre reservas,
-como confirmar, cancelar y procesar reservas.
-
-La implementación completa se realizará en la fase de desarrollo.
-"""
